52bcac0deafc7e2054dd1a651c0b901d6907eec7
JakeOh <jake.oh.lecture@gmail.com>
2023-04-29 16:06:38 +0900

    Add files via upload

---
+++ lab05/src/App.js
@@ -0,0 +1,13 @@
+import IterationExample from './IterationExample';
+
+function App() {
+  const items = ['React', 'Vue.js', 'Angualr.js', 'jQuery'];
+
+  return (
+    <div className="App">
+      <IterationExample items={items} />
+    </div>
+  );
+}
+
+export default App;
