7f0380edc003054382f91c2e52b1260e73015750
JakeOh <jake.oh.lecture@gmail.com>
2023-05-06 11:14:14 +0900

    Add files via upload

--- lab05/src/App.js
+++ lab05/src/App.js
@@ -1,10 +1,19 @@
 import IterationExample from './IterationExample';
+import IterationExample2 from './IterationExample2';
 
 function App() {
-  const items = ['React', 'Vue.js', 'Angualr.js', 'jQuery'];
+  const items = ['React', 'Vue.js', 'Angualar.js', 'jQuery'];
+  const items2 = [
+    { id: 1, name: 'React' },
+    { id: 2, name: 'Vue.js' },
+    { id: 3, name: 'Angular.js' },
+    { id: 4, name: 'jQuery' },
+  ];
 
   return (
     <div className="App">
+      <IterationExample2 items={items2} />
+      <hr />
       <IterationExample items={items} />
     </div>
   );
