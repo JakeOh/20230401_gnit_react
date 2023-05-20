df129a7aa5a7d2d58f948dba6a906bf39db4c89a
JakeOh <jake.oh.lecture@gmail.com>
2023-05-20 14:01:29 +0900

    react-router-dom

--- lab08/src/components/Navigation.js
+++ lab08/src/components/Navigation.js
@@ -6,6 +6,10 @@ const Navigation = () => {
       {/*
       <a href="/">Home</a>
       <a href="/about">About</a>
+      
+      <a> 태그를 사용하면 현재 보고 있는 페이지의 링크를 클릭할 때 마다 
+      새 페이지를 여는 문제가 발생함.
+      react-router-dom 패키지의 Link 컴포넌트를 사용하면 현재 페이지를 다시 요청하지 않음.
       */}
 
       <Link to="/">Home</Link>
