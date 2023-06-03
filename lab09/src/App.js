3d7e05361fe86da96dd85494805a12c4ef95a3e4
JakeOh <jake.oh.lecture@gmail.com>
2023-06-03 15:02:01 +0900

    Add files via upload

--- lab09/src/App.js
+++ lab09/src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
 import PostsPage from './pages/PostsPage';
+import PostDetailsPage from './pages/PostDetailsPage';
 
 const App = () => {
   return (
@@ -8,6 +9,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/posts" element={<PostsPage />} />
+        <Route path="/posts/:id" element={<PostDetailsPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ lab09/src/pages/PostDetailsPage.js
@@ -0,0 +1,9 @@
+const PostDetailsPage = () => {
+  return (
+    <div>
+      <h1>포스트 상세보기 페이지</h1>
+    </div>
+  );
+};
+
+export default PostDetailsPage;
