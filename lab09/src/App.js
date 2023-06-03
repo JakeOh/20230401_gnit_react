2228edb2d7880061ddd348499e3dd22804eedca0
JakeOh <jake.oh.lecture@gmail.com>
2023-06-03 12:21:40 +0900

    react redux

---
+++ lab09/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import PostsPage from './pages/PostsPage';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/posts" element={<PostsPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
---
+++ lab09/src/pages/PostsPage.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const PostsPage = () => {
+  return (
+    <div>
+      <h1>포스트 목록</h1>
+
+      <Link to="/">Dashboard</Link>
+    </div>
+  );
+};
+
+export default PostsPage;
